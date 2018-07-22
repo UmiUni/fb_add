770de40d27d2aef7581081f5eeb1fbef038fd72a
easonsun <ysun73@illinois.edu>
2018-07-21 22:00:27 -0700

    remove some non chinese names

--- main.py
+++ main.py
@@ -12,6 +12,15 @@ import win32clipboard
 from collections import deque
 import random
 from utils import gui_bot
+try:
+    import cPickle as pickle
+except ModuleNotFoundError:
+    import pickle
+import os.path
+
+def exitAndAlert():
+    print("friend request queue reaches 1000!!")
+    sys.exit()
 
 '''
 im1 = pyautogui.screenshot()
@@ -47,89 +56,112 @@ for f in os.listdir(needleImageDir):
     ref.append(needleImage)
 SimpleIdentifier = SimpleIdentifier(ref, needleImage.shape[0], needleImage.shape[1])
 
-friendIdQ = deque()
-friendIdQ.append('superchaoran')  # 'https://www.facebook.com/superchaoran/friends'
-for depth in range(3):
-    while (len(friendIdQ) != 0):
-        curPage = 'https://www.facebook.com/' + friendIdQ.popleft() + '/friends'
-        print(curPage)
-        pyautogui.hotkey('ctrl', 'l')
-        # print (list(curPage))
-        pyautogui.press(list(curPage))
-        time.sleep(random.uniform(0.5, 2))
+friendIdQFilePath = "friendIdQFile.pkl"
+friendIdQFile = open(friendIdQFilePath, 'ab+')
+friendIdQ = None
+friendRequestNumPath = "friendRequestNum.pkl"
+friendRequestNumFile = open(friendIdQFilePath, 'ab+')
+friendRequestNum = None
+# if os.path.isfile(friendIdQFilePath):
+try:
+    friendIdQ = pickle.load(friendIdQFile)
+    friendRequestNum = pickle.load(friendRequestNumFile)
+except EOFError:
+    friendIdQ = deque()
+    friendIdQ.append('superchaoran')  # 'https://www.facebook.com/superchaoran/friends'
+    friendRequestNum = 247
+# else:
+#     friendIdQ = deque()
+#     friendIdQ.append('superchaoran')  # 'https://www.facebook.com/superchaoran/friends'
+# for depth in range(3):
+while (len(friendIdQ) != 0):
+    curPage = 'https://www.facebook.com/' + friendIdQ.popleft() + '/friends'
+    print(curPage)
+    pyautogui.hotkey('ctrl', 'l')
+    # print (list(curPage))
+    pyautogui.press(list(curPage))
+    time.sleep(random.uniform(0.5, 2))
+    if not 'chaoran' in curPage:
         pyautogui.press('enter')
-        time.sleep(random.uniform(1.5, 4))
+    time.sleep(random.uniform(1.5, 4))
 
-        noFriendFoundCtr = 0
-        while (noFriendFoundCtr < 5):
-            haystackImage = pyautogui.screenshot()
-            print(haystackImage.size)
-            haystackFileObj = None
-            if isinstance(haystackImage, str):
-                # 'image' is a filename, load the Image object
-                haystackFileObj = open(haystackImage, 'rb')
-                haystackImage = Image.open(haystackFileObj)
-            haystackImage = np.array(haystackImage)[:, :, :3]
+    noFriendFoundCtr = 0
+    while (noFriendFoundCtr < 5):
+        haystackImage = pyautogui.screenshot()
+        print(haystackImage.size)
+        haystackFileObj = None
+        if isinstance(haystackImage, str):
+            # 'image' is a filename, load the Image object
+            haystackFileObj = open(haystackImage, 'rb')
+            haystackImage = Image.open(haystackFileObj)
+        haystackImage = np.array(haystackImage)[:, :, :3]
 
-            # print ( tuple(needleImage[0,0]))	#(66, 103, 178)
-            fbBlue_addButton = tuple(needleImage[2, 2])
-            # fbBlue = (66, 103, 178)
-            # print(fbBlue_addButton)
-            fbBlue_nameBox = (54, 88, 153);
-            getRect = GetRectangles(haystackImage, fbBlue_addButton)
-            rects = getRect.getRectangles()
-            target_rects = SimpleIdentifier.getTargetPositions(haystackImage, rects)
-            print(target_rects)
+        # print ( tuple(needleImage[0,0]))	#(66, 103, 178)
+        fbBlue_addButton = tuple(needleImage[2, 2])
+        # fbBlue = (66, 103, 178)
+        # print(fbBlue_addButton)
+        fbBlue_nameBox = (54, 88, 153);
+        getRect = GetRectangles(haystackImage, fbBlue_addButton)
+        rects = getRect.getRectangles()
+        target_rects = SimpleIdentifier.getTargetPositions(haystackImage, rects)
+        print(target_rects)
 
-            # ctr = 0
-            noFriendFound = True
-            for target_rect in target_rects:
-                # print(location)
-                button_x, button_y = getCenter(target_rect)  # pyautogui.center(location)
+        # ctr = 0
+        noFriendFound = True
+        for target_rect in target_rects:
+            # print(location)
+            button_x, button_y = getCenter(target_rect)  # pyautogui.center(location)
+            gui_bot.random_move_to(button_x, button_y)
+
+            try:
+                top, bottom, left, right = target_rect
+                height, width = getDim(target_rect)
+                # move to add friend button top left corner
+                nameBoxRight = int(button_x - width / 2)
+                nameBoxLeft = int(nameBoxRight - width * 1.8)
+                nameBoxTop = int(button_y - height / 2 - height * 1.5)
+                nameBoxButtom = int(button_y + height / 2 + height * 1.5)
+                nameBox = haystackImage[nameBoxTop: nameBoxButtom, nameBoxLeft: nameBoxRight]
+            except ValueError:
+                continue
+            Image.fromarray(nameBox)  # .show()#save('name_box/'+str(ctr)+'.png', "PNG") #show()
+            try:
+                isChinese = ocr(nameBox)
+            except ValueError:
+                continue
+            if (isChinese):
+                #print("yes")
+                name_x, name_y = findFirstBlue(nameBox, nameBoxLeft, nameBoxTop, fbBlue_nameBox)
+                gui_bot.random_move_to(name_x, name_y)
+                # copy link
+                pyautogui.click(button='right')
+                for i in range(5):
+                    pyautogui.press('down')
+                pyautogui.press('enter')
+                # read from clipboard and process
+                win32clipboard.OpenClipboard()
+                data = win32clipboard.GetClipboardData()
+                win32clipboard.CloseClipboard()
+                friend_id = (data.split('?')[0]).split('/')[-1]
+                # print (friend_id)
+                # add to queue
+                friendIdQ.append(friend_id)
+                pickle.dump(friendIdQ, friendIdQFile)
+                friendRequestNum += 1
+                pickle.dump(friendRequestNum, friendRequestNumFile)
+                if (friendRequestNum == 1000):
+                    exitAndAlert()
                 gui_bot.random_move_to(button_x, button_y)
+                pyautogui.click(button_x, button_y)
+            noFriendFound = False
+        # ctr += 1
+        # break
+        if (noFriendFound == True):
+            noFriendFoundCtr += 1
+        del haystackImage
+        del getRect
+        del rects
+        del target_rects
+        pyautogui.scroll(random.randrange(-1500, -500))
+        time.sleep(random.uniform(0.5, 2))
 
-                try:
-                    top, bottom, left, right = target_rect
-                    height, width = getDim(target_rect)
-                    # move to add friend button top left corner
-                    nameBoxRight = int(button_x - width / 2)
-                    nameBoxLeft = int(nameBoxRight - width * 1.8)
-                    nameBoxTop = int(button_y - height / 2 - height * 1.5)
-                    nameBoxButtom = int(button_y + height / 2 + height * 1.5)
-                    nameBox = haystackImage[nameBoxTop: nameBoxButtom, nameBoxLeft: nameBoxRight]
-                except ValueError:
-                    continue
-                Image.fromarray(nameBox)  # .show()#save('name_box/'+str(ctr)+'.png', "PNG") #show()
-                try:
-                    isChinese = ocr(nameBox)
-                except ValueError:
-                    continue
-                if (isChinese):
-                    print("yes")
-                    name_x, name_y = findFirstBlue(nameBox, nameBoxLeft, nameBoxTop, fbBlue_nameBox)
-                    gui_bot.random_move_to(name_x, name_y)
-                    # copy link
-                    pyautogui.click(button='right')
-                    for i in range(5):
-                        pyautogui.press('down')
-                    pyautogui.press('enter')
-                    # read from clipboard and process
-                    win32clipboard.OpenClipboard()
-                    data = win32clipboard.GetClipboardData()
-                    win32clipboard.CloseClipboard()
-                    friend_id = (data.split('?')[0]).split('/')[-1]
-                    # print (friend_id)
-                    # add to queue
-                    friendIdQ.append(friend_id)
-                    #pyautogui.click(button_y, button_x)
-                noFriendFound = False
-            # ctr += 1
-            # break
-            if (noFriendFound == True):
-                noFriendFoundCtr += 1
-            del haystackImage
-            del getRect
-            del rects
-            del target_rects
-            pyautogui.scroll(random.randrange(-1500, -500))
-            time.sleep(random.uniform(0.5, 2))
